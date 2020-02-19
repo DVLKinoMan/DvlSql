9b43cd17b7bb5f63d07ac9addb740395859d9851
D.Dvali <dvali.daviti@gmail.com>
2020-02-19 18:31:53 +0400

    Mock in ToList More Elegant Code

--- Dvl_Sql.Tests/Result/ToList.cs
+++ Dvl_Sql.Tests/Result/ToList.cs
@@ -19,45 +19,57 @@ namespace Dvl_Sql.Tests.Result
 
         private string TableName = "dbo.Words";
 
+        private Mock<IDataReader> CreateDataReaderMock<T>(List<T> list)
+        {
+            var readerMoq = new Mock<IDataReader>();
+            int index = 0;
+
+            readerMoq.Setup(reader => reader.Read())
+                .Returns(() => index < list.Count);
+            // .Callback(() => { index++; });
+
+            readerMoq.Setup(reader => reader[0])
+                .Returns(() => list[index++]);
+
+
+            return readerMoq;
+        }
+
+        private Mock<IDvlSqlCommand> CreateSqlCommandMock<T>(Mock<IDataReader> readerMoq)
+        {
+            var commandMoq = new Mock<IDvlSqlCommand>();
+
+            commandMoq.Setup(com => com.ExecuteReaderAsync(It.IsAny<Func<IDataReader, T>>(),
+                    It.IsAny<int?>(),
+                    It.IsAny<CommandBehavior>(), It.IsAny<CancellationToken>()))
+                .Returns((Func<IDataReader, T> func2, int? timeout,
+                        CommandBehavior behavior, CancellationToken cancellationToken) =>
+                    Task.FromResult(func2(readerMoq.Object)));
+
+            return commandMoq;
+        }
+
+        private Mock<IDvlSqlConnection> CreateConnection<T>(Mock<IDvlSqlCommand> commandMoq)
+        {
+            var moq = new Mock<IDvlSqlConnection>();
+
+            moq.Setup(m => m.ConnectAsync(It.IsAny<Func<IDvlSqlCommand, Task<T>>>(),
+                    It.IsAny<string>(), It.Is<CommandType>(com => com == CommandType.Text),
+                    It.IsAny<SqlParameter[]>()))
+                .Returns((Func<IDvlSqlCommand, Task<T>> func, string sqlString,
+                    CommandType commandType, SqlParameter[] parameters) => func(commandMoq.Object));
+
+            return moq;
+        }
+
         [Test]
         public void TestMethod1()
         {
-            var moq = new Mock<IDvlSqlConnection>();
-            // .Returns(Task.FromResult(new List<int>(){1,1,1}));
+            var list2 = new List<int>() {1, 2, 3};
 
-            moq.Setup(m => m.ConnectAsync(It.IsAny<Func<IDvlSqlCommand, Task<List<int>>>>(),
-                    It.IsAny<string>(), It.Is<CommandType>(com => com == CommandType.Text),
-                    It.IsAny<SqlParameter[]>()))
-                .Returns((Func<IDvlSqlCommand, Task<List<int>>> func, string sqlString,
-                    CommandType commandType, SqlParameter[] parameters) =>
-                {
-                    var commandMoq = new Mock<IDvlSqlCommand>();
-                    var readerMoq = new Mock<IDataReader>();
-                    var list2 = new List<int>() {1, 2, 3};
-                    int index = 0;
-
-                    readerMoq.Setup(reader => reader.Read())
-                        .Returns(() => index < list2.Count);
-                    // .Callback(() => { index++; });
-
-                    readerMoq.Setup(reader => reader[0])
-                        .Returns(() => list2[index++]);
-
-                    commandMoq.Setup(com => com.ExecuteReaderAsync(It.IsAny<Func<IDataReader, List<int>>>(),
-                            It.IsAny<int?>(),
-                            It.IsAny<CommandBehavior>(), It.IsAny<CancellationToken>()))
-                        .Returns((Func<IDataReader, List<int>> func2, int? timeout,
-                                CommandBehavior behavior, CancellationToken cancellationToken) =>
-                            Task.FromResult(func2(readerMoq.Object)));
-
-                    return func(commandMoq.Object);
-
-                    int Get() => list2[index];
-                });
-            // .Returns(() => Task.FromResult(new List<int>() {1, 2, 3}));
-
-            // Assert.That(moq.Object.ConnectAsync(f => f.ExecuteReaderAsync(AsList(r => (int) r[0])),
-            //     "addd").Result, Is.EquivalentTo(new List<int>() {1, 2, 3}));
+            var readerMoq = CreateDataReaderMock(list2);
+            var commandMoq = CreateSqlCommandMock<List<int>>(readerMoq);
+            var moq = CreateConnection<List<int>>(commandMoq);
 
             var list = IDvlSql.DefaultDvlSql(moq.Object)
                 .From(TableName)
@@ -66,6 +78,24 @@ namespace Dvl_Sql.Tests.Result
                 .Result;
 
             Assert.That(list, Is.EquivalentTo(new List<int>() {2, 3, 4}));
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            var list2 = new List<string>() {"David", "Lasha", "SomeGuy"};
+
+            var readerMoq = CreateDataReaderMock(list2);
+            var commandMoq = CreateSqlCommandMock<List<string>>(readerMoq);
+            var moq = CreateConnection<List<string>>(commandMoq);
+
+            var list = IDvlSql.DefaultDvlSql(moq.Object)
+                .From(TableName)
+                .Select()
+                .ToListAsync(r => ((string) r[0]).Substring(0, 1))
+                .Result;
+
+            Assert.That(list, Is.EquivalentTo(new List<string>() {"D", "L", "S"}));
         }
     }
 }
