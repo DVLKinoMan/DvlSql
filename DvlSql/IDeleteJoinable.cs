b1c9415f7b505cb5f0fb8a31c3c631e779b7d9ad
Daviti Dvali <dvali.daviti@gmail.com>
2024-09-15 14:43:40 +0400

    Generic join methods

--- DvlSql/IDeleteJoinable.cs
+++ DvlSql/IDeleteJoinable.cs
@@ -5,11 +5,11 @@ namespace DvlSql;
 public interface IDeleteJoinable : IDeleteOutputable<int>, IInsertDeleteExecutable<int>
 {
     IDeleteJoinable Join<T>(string tableName, DvlSqlComparisonExpression<T> compExpression);
-    IDeleteJoinable Join(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
+    IDeleteJoinable Join<T>(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
     IDeleteJoinable FullJoin<T>(string tableName, DvlSqlComparisonExpression<T> compExpression);
-    IDeleteJoinable FullJoin(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
+    IDeleteJoinable FullJoin<T>(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
     IDeleteJoinable LeftJoin<T>(string tableName, DvlSqlComparisonExpression<T> compExpression);
-    IDeleteJoinable LeftJoin(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
+    IDeleteJoinable LeftJoin<T>(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
     IDeleteJoinable RightJoin<T>(string tableName, DvlSqlComparisonExpression<T> compExpression);
-    IDeleteJoinable RightJoin(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
+    IDeleteJoinable RightJoin<T>(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
 }
--- DvlSql/IJoinable.cs
+++ DvlSql/IJoinable.cs
@@ -6,11 +6,11 @@ namespace DvlSql;
 public interface IJoinable
 {
     ISelector Join<T>(string tableName, DvlSqlComparisonExpression<T> compExpression);
-    ISelector Join(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
+    ISelector Join<T>(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
     ISelector FullJoin<T>(string tableName, DvlSqlComparisonExpression<T> compExpression);
-    ISelector FullJoin(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
+    ISelector FullJoin<T>(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
     ISelector LeftJoin<T>(string tableName, DvlSqlComparisonExpression<T> compExpression);
-    ISelector LeftJoin(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
+    ISelector LeftJoin<T>(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
     ISelector RightJoin<T>(string tableName, DvlSqlComparisonExpression<T> compExpression);
-    ISelector RightJoin(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
+    ISelector RightJoin<T>(string tableName, string firstTableMatchingCol, string secondTableMatchingCol);
 }
